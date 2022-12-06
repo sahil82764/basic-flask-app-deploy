69e4d7ed956ca34c10bbcb320898ffa675a953d1
sahil82764 <sahil82764@gmail.com>
2022-12-06 17:36:43 +0530

    new commit port

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,5 @@ COPY . /app
 WORKDIR /app
 RUN pip install -r requirements.txt
 EXPOSE $PORT
+ENTRYPOINT [ "python" ]
 CMD gunicorn --workers=4 --bind 0.0.0.0:$PORT app:app
--- app.py
+++ app.py
@@ -2,10 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-app.route('/', methods=['GET', 'POST'])
-
+@app.route('/', methods=['GET'])
 def index():
     return "Startup Page"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host ='0.0.0.0', port = 5000,debug=True)
